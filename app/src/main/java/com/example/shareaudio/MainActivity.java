23822b1cd3f541981f14ac8cf416dfcd5de1f94d
Matt Taylor <mht3@illinois.edu>
2020-04-20 15:11:35 -0500

    Fixed relative layout error

--- app/src/main/java/com/example/shareaudio/MainActivity.java
+++ app/src/main/java/com/example/shareaudio/MainActivity.java
@@ -68,8 +68,8 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.layout.activity_main, menu);
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 }
