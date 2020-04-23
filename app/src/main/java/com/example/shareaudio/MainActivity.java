65d3590dcb8e503d95e4762e6816bb1ded26b7d4
Matt Taylor <mht3@illinois.edu>
2020-04-22 21:42:19 -0500

    Added a dope red background

--- app/src/main/java/com/example/shareaudio/MainActivity.java
+++ app/src/main/java/com/example/shareaudio/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         final TextView out = findViewById(R.id.out);
         final Button button1 = findViewById(R.id.button1);
