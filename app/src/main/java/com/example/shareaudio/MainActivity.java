d674c8d21ba9adf05e68cd6dff27be93f04bfcbe
Eric Monson <emonson2@illinois.edu>
2020-05-01 12:22:39 -0500

    Implemented the pairing feature in the ListView

--- app/src/main/java/com/example/shareaudio/MainActivity.java
+++ app/src/main/java/com/example/shareaudio/MainActivity.java
@@ -13,6 +13,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -25,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVERABLE_BT = 0;
     private static final String TAG = "MainActivity";
@@ -45,11 +46,27 @@ public class MainActivity extends Activity {
     private DeviceListAdapter deviceListAdapter;
 
     @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called.");
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver2);
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         devices = new ArrayList<>();
         setContentView(R.layout.activity_main);
+
+        //Broadcasts when band state changes (ie:pairing)
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+        registerReceiver(broadcastReceiver2, filter);
+
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        listView.setOnItemClickListener(MainActivity.this);
+
         final Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         final ImageView logo = findViewById(R.id.logo);
@@ -132,6 +149,30 @@ public class MainActivity extends Activity {
         }
     };
 
+    private final BroadcastReceiver broadcastReceiver2 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if(action.equals(BluetoothDevice.ACTION_BOND_STATE_CHANGED)) {
+                BluetoothDevice mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //3 cases:
+                //case 1:
+                if (mDevice.getBondState() == BluetoothDevice.BOND_BONDED) {
+                    Log.d(TAG, "BroadcastReceiver: BOUND_BONDED.");
+                }
+                //case 2:
+                if (mDevice.getBondState() == BluetoothDevice.BOND_BONDING) {
+                    Log.d(TAG, "BroadcastReceiver: BOUND_BONDING.");
+                }
+                //case 3:
+                if (mDevice.getBondState() == BluetoothDevice.BOND_NONE) {
+                    Log.d(TAG, "BroadcastReceiver: BOUND_NONE.");
+                }
+            }
+        }
+    };
+
     public void discover(View v) {
         Toast.makeText(getApplicationContext(), "Searching", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "Searching");
@@ -152,6 +193,22 @@ public class MainActivity extends Activity {
             IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
             registerReceiver(broadcastReceiver, discoverDevicesIntent);
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //first cancel discovery because its very memory intensive.
+        bluetoothAdapter.cancelDiscovery();
+
+        Log.d(TAG, "onItemClick: You Clicked on a device.");
+        String deviceName = devices.get(position).getName();
+        String deviceAddress = devices.get(position).getAddress();
+        Log.d(TAG, "onItemClick: deviceName = " + deviceName);
+        Log.d(TAG, "onItemClick: deviceAddress = " + deviceAddress);
+
+        //create the bond.
+        Log.d(TAG, "Trying to pair with " + deviceName);
+        devices.get(position).createBond();
     }
 //    public void setPairedDevices(View v) {
 //        deviceSet = bluetoothAdapter.getBondedDevices();
